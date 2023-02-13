d0c1ccf35bcab57c2e2688174dfcd9b932ae5484
MidoRoshdy <117544553+MidoRoshdy@users.noreply.github.com>
2023-02-13 23:49:36 +0200

    second commit

--- lib/screens/OnBoarding/provider/OnBoardingProvider.dart
+++ lib/screens/OnBoarding/provider/OnBoardingProvider.dart
@@ -3,14 +3,4 @@ import 'package:flutter/cupertino.dart';
 
 class OnBoardingProvider extends ChangeNotifier {
   OnBoardingState state = OnBoardingState();
-
-  // Future<void> onBoardFinish(context) async {
-  //   SharedPreferences shared = await SharedPreferences.getInstance();
-  //   shared.setBool("OnBoarding", true);
-  //   if (context.mounted) {
-  //     Navigator.of(context)
-  //         .pushNamedAndRemoveUntil(App_Routes.home, (route) => false);
-  //     Navigator.of(context).pushNamed(App_Routes.create_account);
-  //   }
-  // }
 }
